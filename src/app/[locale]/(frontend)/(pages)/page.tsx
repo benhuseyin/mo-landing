46d5d17f4249c566615e79a105b971ad934a0bee
Huseyin Kara <67840358+benhuseyin@users.noreply.github.com>
2025-11-11 15:51:58 +0300

    [FEATURE] Create latest musics section

--- src/app/[locale]/(frontend)/(pages)/page.tsx
+++ src/app/[locale]/(frontend)/(pages)/page.tsx
@@ -1,5 +1,6 @@
 import { useTranslations } from "next-intl";
 import HeroSection from "../components/HeroSection";
+import LatestMusicsSection from "../components/LatestMusicsSection";
 
 export default function Home() {
   const t = useTranslations('HomePage');
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main>
       <HeroSection />
+      <LatestMusicsSection />
     </main>
   );
 }
---
+++ src/app/[locale]/(frontend)/components/LatestMusicsSection/index.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import BackgroundImage from "@/src/assets/images/mo-latest-music-cover.webp"
+import { useTranslations } from "next-intl";
+
+const LatestMusicsSection = () => {
+    const t = useTranslations('HomePage');
+
+    const title = t.rich('latest-music.title', {
+        br: () => <br />
+    });
+
+
+    return (
+        <section className="relative">
+            <div className="absolute top-1/2 text-[50px] font-bold p-5 sm:p-10">
+                <h2>{title}</h2>
+                <iframe data-testid="embed-iframe" style={{ borderRadius: "12px" }} src="https://open.spotify.com/embed/track/1IS7T8BYibhyAGxX8Jcgbr?utm_source=generator" width="100%" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            </div>
+
+            <Image src={BackgroundImage} alt="Mahmut Orhan Latest Release" className="max-h-[600px] w-full object-cover" />
+        </section>
+    );
+}
+
+export default LatestMusicsSection
