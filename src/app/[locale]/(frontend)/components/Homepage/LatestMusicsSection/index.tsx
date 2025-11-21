8ab620faaa13863a21a484914fe453d7e800990a
Huseyin Kara <67840358+benhuseyin@users.noreply.github.com>
2025-11-21 18:58:51 +0300

    [REFACTOR] Add soundcloud icon

--- src/app/[locale]/(frontend)/components/Homepage/LatestMusicsSection/index.tsx
+++ src/app/[locale]/(frontend)/components/Homepage/LatestMusicsSection/index.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image';
 
 import AppleIcon from '@/src/assets/icons/apple-music.svg'
 import YoutubeIcon from '@/src/assets/icons/youtube-icon.svg'
+import SpotifyIcon from '@/src/assets/icons/spotify-icon.svg'
+import SoundcloudIcon from '@/src/assets/icons/soundcloud-icon.svg'
 
 const LatestReleaseSection = () => {
     const [isHovered, setIsHovered] = useState(false);
@@ -164,9 +166,7 @@ const LatestReleaseSection = () => {
                                 rel="noopener noreferrer"
                                 className="px-4 py-2 bg-green-500/20 hover:bg-green-500/30 rounded-full text-sm text-green-400 hover:text-green-300 transition-all border border-green-500/30 flex items-center gap-2"
                             >
-                                <svg className="w-4 h-4" viewBox="0 0 24 24" fill="currentColor">
-                                    <path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.66 0 12 0zm5.521 17.34c-.24.359-.66.48-1.021.24-2.82-1.74-6.36-2.101-10.561-1.141-.418.122-.779-.179-.899-.539-.12-.421.18-.78.54-.9 4.56-1.021 8.52-.6 11.64 1.32.42.18.479.659.301 1.02zm1.44-3.3c-.301.42-.841.6-1.262.3-3.239-1.98-8.159-2.58-11.939-1.38-.479.12-1.02-.12-1.14-.6-.12-.48.12-1.021.6-1.141C9.6 9.9 15 10.561 18.72 12.84c.361.181.54.78.241 1.2zm.12-3.36C15.24 8.4 8.82 8.16 5.16 9.301c-.6.179-1.2-.181-1.38-.721-.18-.601.18-1.2.72-1.381 4.26-1.26 11.28-1.02 15.721 1.621.539.3.719 1.02.419 1.56-.299.421-1.02.599-1.559.3z" />
-                                </svg>
+                                <Image src={SpotifyIcon} alt="Apple icon" className="text-[#1DB954] size-4" />
                                 Spotify
                             </a>
                             <a
@@ -182,6 +182,15 @@ const LatestReleaseSection = () => {
                             >
                                 <Image src={YoutubeIcon} alt="Youtube icon" className='size-4' />
                                 YouTube
+                            </a>
+                            <a
+                                href='https://soundcloud.com/mahmut-orhan'
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="px-4 py-2 bg-[#ff5500]/20 hover:bg-[#ff5500]/30 rounded-full text-sm text-[#ff5500] hover:text-[#ff5500] transition-all border border-[#ff5500]/30 flex items-center gap-2"
+                            >
+                                <Image src={SoundcloudIcon} alt="Apple icon" className="text-[#1DB954] size-4" />
+                                Soundcloud
                             </a>
                         </div>
                     </div>
